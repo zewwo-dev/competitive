eb9984c7d1fafadba22d4adbdf12cb41131a0473
Nathaniel <59523827+zewwo-dev@users.noreply.github.com>
2026-02-02 15:18:55 +0900

    fix: add support for aarch

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 dir="/home"
+arch="aarch64" # aarch64, x86_64
 
 if [ -z "$1" ]; then
   cd $dir && mkdir -p build && g++ main.cpp -o build/main -std=c++17 -O2 -Wall -lm --static -pipe && build/main
@@ -8,5 +9,5 @@ elif [ "${1,,}" = "time" ]; then
   cd $dir && mkdir -p build && g++ main.cpp -o build/main -std=c++17 -O2 -Wall -lm --static -pipe
   time build/main
 elif [ "${1,,}" = "precompile" ]; then
-  cd "/usr/include/x86_64-linux-gnu/c++/12/bits" && g++ stdc++.h -std=c++17 -O2
+  cd "/usr/include/$arch-linux-gnu/c++/12/bits" && g++ stdc++.h -std=c++17 -O2
 fi
