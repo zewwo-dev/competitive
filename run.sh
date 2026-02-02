55c893b05b409d7b550946904e59be6a587c6989
Nathaniel <59523827+zewwo-dev@users.noreply.github.com>
2026-02-02 15:37:19 +0900

    chore: update to latest debian

--- run.sh
+++ run.sh
@@ -9,5 +9,5 @@ elif [ "${1,,}" = "time" ]; then
   cd $dir && mkdir -p build && g++ main.cpp -o build/main -std=c++17 -O2 -Wall -lm --static -pipe
   time build/main
 elif [ "${1,,}" = "precompile" ]; then
-  cd "/usr/include/$arch-linux-gnu/c++/12/bits" && g++ stdc++.h -std=c++17 -O2
+  cd "/usr/include/$arch-linux-gnu/c++/14/bits" && g++ stdc++.h -std=c++17 -O2
 fi
